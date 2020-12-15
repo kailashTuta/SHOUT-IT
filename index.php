9e04279e500f411f249f5ded8ff7180b0d648bc0
kailashTuta <kailashTuta@users.noreply.github.com>
2020-12-15 19:06:22 +0530

    Connected frontend to backend

--- index.php
+++ index.php
@@ -29,6 +29,9 @@ $shouts = mysqli_query($con, $query);
             </ul>
         </div>
         <div id="input">
+            <?php if (isset($_GET['error'])) : ?>
+                <div class="error"><?php echo $_GET['error'] ?></div>
+            <?php endif; ?>
             <form method="post" action="process.php">
                 <input type="text" name="user" placeholder="Enter Your Name">
                 <input type="text" name="message" placeholder="Enter A Message">
